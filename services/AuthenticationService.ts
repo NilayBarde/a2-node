72bc0f14cd4ea77d546034ae88ae65e90a62436e
Nilay Barde <nilaybarde@gmail.com>
2022-02-20 19:36:40 -0500

    Update DB name and DB username and password

--- services/AuthenticationService.ts
+++ services/AuthenticationService.ts
@@ -4,72 +4,70 @@ import mongoose from "mongoose";
 const userDao: UserDao = UserDao.getInstance();
 
 const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
-const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
+const DB_USERNAME = "nilaybarde"; //process.env.DB_USERNAME;
+const DB_PASSWORD = "oPpleA9vA3IPwAlE"; //process.env.DB_PASSWORD;
+const HOST = "cluster0.jarvx.mongodb.net";
+const DB_NAME = "cs5500-a1-db";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
 mongoose.connect(connectionString);
 
-
 export const login = (u: string, p: string) =>
-  userDao.findUserByCredentials(u, p)
-    .then(user => {
-      if (user) {
-        return user;
-      } else {
-        throw "Unknown user"
-      }
-    })
-    .then(user => user)
-    .catch(e => e)
+    userDao
+        .findUserByCredentials(u, p)
+        .then((user) => {
+            if (user) {
+                return user;
+            } else {
+                throw "Unknown user";
+            }
+        })
+        .then((user) => user)
+        .catch((e) => e);
 
 export const register = (u: string, p: string, e: string) =>
-  userDao.findUserByUsername(u)
-    .then(user => {
-      if (user) {
-        throw 'User already exists';
-      } else {
-        return userDao.createUser({
-          username: u, password: p, email: e
-        });
-      }
-    })
-    .then(newUser => newUser)
-    .catch(e => e);
+    userDao
+        .findUserByUsername(u)
+        .then((user) => {
+            if (user) {
+                throw "User already exists";
+            } else {
+                return userDao.createUser({
+                    username: u,
+                    password: p,
+                    email: e,
+                });
+            }
+        })
+        .then((newUser) => newUser)
+        .catch((e) => e);
 
 export const initializeSalaries = (salary: number) => {
-  return userDao.findAllUsers()
-    .then(users => {
-      const sPromises = users.map(user =>
-        userDao.updateUserSalaryByUsername(user.username, salary));
-      const resultPromise = Promise.all(sPromises);
-      resultPromise
-        .then(values => {
-          return values
-        })
-    })
-}
+    return userDao.findAllUsers().then((users) => {
+        const sPromises = users.map((user) =>
+            userDao.updateUserSalaryByUsername(user.username, salary)
+        );
+        const resultPromise = Promise.all(sPromises);
+        resultPromise.then((values) => {
+            return values;
+        });
+    });
+};
 
 export const giveRaise = (raise: number) => {
-  return userDao.findAllUsers()
-    .then(users => {
-      const salaryPromises = users.map(user => {
-        // @ts-ignore
-        const newSalary = user.salary * (1 + raise / 100);
-        return userDao.updateUserSalaryByUsername(
-          user.username,
-          newSalary)
-      });
-      const resultPromise = Promise.all(salaryPromises);
-      resultPromise
-        .then(values => {
-          return values;
-        })
-    })
-}
+    return userDao.findAllUsers().then((users) => {
+        const salaryPromises = users.map((user) => {
+            // @ts-ignore
+            const newSalary = user.salary * (1 + raise / 100);
+            return userDao.updateUserSalaryByUsername(user.username, newSalary);
+        });
+        const resultPromise = Promise.all(salaryPromises);
+        resultPromise.then((values) => {
+            return values;
+        });
+    });
+};
 
 // giveRaise(50)
 //   .then(results => console.log(results));
@@ -80,8 +78,7 @@ export const giveRaise = (raise: number) => {
 // register('alice008', 'alice234', 'alice234@gmail.com')
 //   .then(user => console.log(user))
 //
-login('alice008', 'alice234')
-  .then(user => console.log(user))
+login("alice008", "alice234").then((user) => console.log(user));
 
 // userDao.findAllUsers()
 //   .then(users => console.log(users));
--- services/AuthenticationService2.ts
+++ services/AuthenticationService2.ts
@@ -6,50 +6,54 @@ const userDao: UserDao = UserDao.getInstance();
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
+const HOST = "cluster0.jarvx.mongodb.net";
+const DB_NAME = "cs5500-a1-db";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
 mongoose.connect(connectionString);
 
-
 export const login = async (u: string, p: string) => {
-  try {
-    const user = await userDao.findUserByCredentials(u, p);
-    if (!user) {
-      throw "Unknown user";
+    try {
+        const user = await userDao.findUserByCredentials(u, p);
+        if (!user) {
+            throw "Unknown user";
+        }
+        return user;
+    } catch (e) {
+        return e;
     }
-    return user;
-  } catch (e) {
-    return e;
-  }
-}
+};
 
 export const register = async (u: string, p: string, e: string) => {
-  try {
-    const user = await userDao.findUserByUsername(u);
-    if (user) {
-      throw 'User already exists';
+    try {
+        const user = await userDao.findUserByUsername(u);
+        if (user) {
+            throw "User already exists";
+        }
+        const newUser = await userDao.createUser({
+            username: u,
+            password: p,
+            email: e,
+        });
+        return newUser;
+    } catch (e) {
+        return e;
     }
-    const newUser = await userDao.createUser({username: u, password: p, email: e});
-    return newUser;
-  } catch (e) {
-    return e;
-  }
-}
+};
 
 export const initializeSalaries = async (salary: number) => {
-  const users = await userDao.findAllUsers()
-  const salaryPromises = users.map(user =>
-    userDao.updateUserSalaryByUsername(user.username, salary));
-  const values = await Promise.all(salaryPromises);
-  return values;
-}
+    const users = await userDao.findAllUsers();
+    const salaryPromises = users.map((user) =>
+        userDao.updateUserSalaryByUsername(user.username, salary)
+    );
+    const values = await Promise.all(salaryPromises);
+    return values;
+};
 
-register('alice678', 'alice234', 'alice234@gmail.com')
+register("alice678", "alice234", "alice234@gmail.com");
 
-login('alice678', 'alice234')
+login("alice678", "alice234");
 // login('alice', 'alice123')
 
 // userDao.findAllUsers()
