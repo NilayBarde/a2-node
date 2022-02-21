c36a63836dcd9a90459c480e7c627fb7923d0592
Nilay Barde <nilaybarde@gmail.com>
2022-02-20 19:19:31 -0500

    Fix examples

--- examples/4.2/example3.ts
+++ examples/4.2/example3.ts
@@ -1,20 +1,25 @@
-import makePromise1 from './promiseMaker'
+import makePromise1 from "../promiseMaker";
 
-console.log("main handler starting")
+console.log("main handler starting");
 
 function driver(promiseName: string, flag: boolean) {
-    console.log(`starting driver(${flag})`)
-    makePromise1(promiseName,flag,10)
-        .then(n => {console.log(`promise ${promiseName} fulfilled and passed ${n} to its successor`);
-                    return n+1
-                })
-        .then(m => console.log(`the second then block received ${m}`))
-        .catch(n => console.log(`promise ${promiseName} rejected and passed "${n}" to its successor`))
+    console.log(`starting driver(${flag})`);
+    makePromise1(promiseName, flag, 10)
+        .then((n) => {
+            console.log(
+                `promise ${promiseName} fulfilled and passed ${n} to its successor`
+            );
+            return n + 1;
+        })
+        .then((m) => console.log(`the second then block received ${m}`))
+        .catch((n) =>
+            console.log(
+                `promise ${promiseName} rejected and passed "${n}" to its successor`
+            )
+        );
 }
 
- 
-driver("promise1",true)
-driver("promise2",false)
+driver("promise1", true);
+driver("promise2", false);
 
-console.log('main handler finished')
-
+console.log("main handler finished");
--- examples/4.2/example7.ts
+++ examples/4.2/example7.ts
@@ -1,17 +1,20 @@
-import myPromise from './promiseMaker'
+// import myPromise from "../promiseMaker";
 
-async function driver(flag: boolean) {
-    try {
-        console.log(`starting driver(${flag})`)
-        const n = await myPromise(flag)
-        console.log(`myPromise(${flag}) fulfilled and passed ${n} to its successor`);
-    } catch (n) {
-        console.log(`myPromise(${flag}) rejected and passed "${n}" to its successor`)
-    }
-}
+// async function driver(flag: boolean) {
+//     try {
+//         console.log(`starting driver(${flag})`);
+//         const n = await myPromise(flag);
+//         console.log(
+//             `myPromise(${flag}) fulfilled and passed ${n} to its successor`
+//         );
+//     } catch (n) {
+//         console.log(
+//             `myPromise(${flag}) rejected and passed "${n}" to its successor`
+//         );
+//     }
+// }
 
+// driver(true);
+// driver(false);
 
-driver(true)
-driver(false)
-
-console.log('main thread finished')
+// console.log("main thread finished");
