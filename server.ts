c2205897804c81516a68fdfbfe44a4bfdea0d154
Nilay Barde <nilaybarde@gmail.com>
2022-04-02 21:34:24 -0400

    Forgot http

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ mongoose.connect(connectionString);
 const app = express();
 app.use(cors({
 	credentials: true,
-	origin: ["http://localhost:3000", "raceful-llama-314586.netlify.app"]
+	origin: ["http://localhost:3000", "http://raceful-llama-314586.netlify.app"]
 }))
 app.use(function (req: Request, res: Response, next ) {
     res.setHeader('Access-Control-Allow-Origin', req.header('origin') || '*');
